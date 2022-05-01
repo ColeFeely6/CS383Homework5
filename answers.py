6d3c7f8ece2ebeea907e0af718f6ba8088e56568
ColeFeely6 <colehfeely@gmail.com>
2022-04-30 21:50:05 -0400

    fix mult choice for q4

--- answers.py
+++ answers.py
@@ -100,7 +100,7 @@ def q4():
     """
     Enter one of the following answer choices below: "a", "b", "c", "d"
     """
-    changed_policy_answer = 'a'
+    changed_policy_answer = 'b'
 
     return {"gridworld": gridworld, "EPSILON": EPSILON,
         "discount_factor": discount_factor,"utilities": utilities,
